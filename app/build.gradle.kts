de7965fe90959ae431d6e3d0241b13b55f9dda32
locshadow03 <locchuvan89@gmail.com>
2024-08-12 17:10:45 +0700

    loc add fragment_edit_username.xml, update drawable and build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,18 +37,13 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
-    implementation(libs.navigation.fragment)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-
-    implementation(libs.constraintlayout)
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
